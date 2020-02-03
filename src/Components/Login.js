c35b395b8bb2ca4d9aa040733f118ffb895341f6
owaiss22 <owaisiqbalhussain@gmail.com>
2020-02-04 01:21:34 +0500

    login screen complete

---
+++ src/Components/Login.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+
+export default class Login extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <Container>
+                    <Row>
+                        <Col sm={4}></Col>
+                        <Col sm={4}>
+                            <h1 style={{marginTop:'150px',textAlign:'center'}}>Login</h1>
+                            <br/>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="Enter email"
+                                        name="email"    
+                                    />
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Password"
+                                        name="password"
+                                    />
+                                </Form.Group>
+                            </Form>
+                            <Button style={{width:'100%',fontSize:'20px'}} variant="success">Login</Button>
+                            <br/>
+                            <br/>
+                            <Button style={{width:'100%',fontSize:'20px'}} variant="danger">Signup</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </React.Fragment>
+        )
+    }
+}
